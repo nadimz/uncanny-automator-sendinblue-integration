c9bc50572b005990e668ffc719e0ada8b5122db7
Nadim Zubidat <nadim.zubidat@outlook.com>
2023-01-13 14:35:49 +0000

    add sendinblue sdk

--- sendinblue/actions/sendinblue-add-user-to-waiting-list-action.php
+++ sendinblue/actions/sendinblue-add-user-to-waiting-list-action.php
@@ -3,13 +3,13 @@
 use Uncanny_Automator\Recipe;
 
 /**
- * Class Sendinblue_Create_Contact_Action
+ * Class Sendinblue_Add_User_To_Waiting_List
  */
-class Sendinblue_Create_Contact_Action {
+class Sendinblue_Add_User_To_Waiting_List_Action {
 	use Recipe\Actions;
 
 	/**
-	 * Sendinblue_Create_Contact_Action constructor.
+	 * Sendinblue_Add_User_To_Waiting_Action constructor.
 	 */
 	public function __construct() {		
 		$this->setup_action();
@@ -28,13 +28,13 @@ class Sendinblue_Create_Contact_Action {
 	protected function setup_action() {
 
 		$this->set_integration( 'SENDINBLUE' );
-		$this->set_action_code( 'SB_CREATE_CONTACT' );
+		$this->set_action_code( 'SB_ADD_USER_TO_WAITING_LIST' );
 		$this->set_requires_user( false );
 		$this->set_action_meta( 'SB_EMAIL' );
 		/* translators: Action - WordPress */
-		$this->set_sentence( sprintf( esc_attr__( 'Create a contact from {{contact:%1$s}}', 'automator-sendinblue-integration' ), $this->get_action_meta() ) );
+		$this->set_sentence( sprintf( esc_attr__( 'Add {{contact:%1$s}} to a waiting list', 'automator-sendinblue-integration' ), $this->get_action_meta() ) );
 		/* translators: Action - WordPress */
-		$this->set_readable_sentence( esc_attr__( 'Create a {{contact}}', 'automator-sendinblue-integration' ) );
+		$this->set_readable_sentence( esc_attr__( 'Add a {{contact}} to a waiting list', 'automator-sendinblue-integration' ) );
 		$options_group = array(
 			$this->get_action_meta() => array(
 				/* translators: Email field */
@@ -64,16 +64,25 @@ class Sendinblue_Create_Contact_Action {
 						'required'    => false,
 					)
 				),
-				/* translators: List ID field */
+				/* translators: Folder ID field */
 				Automator()->helpers->recipe->field->int(
 					array(
-						'option_code' => 'SB_LIST_ID',
-						'label'       => 'List ID',
+						'option_code' => 'SB_FOLDER_ID',
+						'label'       => 'Folder ID',
 						'input_type'  => 'int',
-						'description' => 'The list ID to add this contact to',
-						'required'    => false,
+						'description' => 'The folder ID where waiting list should be created',
+						'required'    => true,
 					)
 				),
+				/* translators: List name field */
+				Automator()->helpers->recipe->field->text(
+					array(
+						'option_code' => 'SB_LIST_NAME',
+						'label'       => esc_attr__( 'List name', 'automator-sendinblue-integration' ),
+						'input_type'  => 'text',
+						'required'    => true,
+					)
+				)
 			),
 		);
 
@@ -91,27 +100,67 @@ class Sendinblue_Create_Contact_Action {
 	 */
 	protected function process_action( $user_id, $action_data, $recipe_id, $args, $parsed ) {
 		$helpers = Automator()->helpers->recipe->sendinblue;
-
+		
 		$email      = $helpers->get_sanitized_email( $parsed[ $this->get_action_meta() ]);
 		$first_name = $helpers->get_sanitized_text( $parsed[ 'SB_FIRSTNAME' ] );
 		$last_name  = $helpers->get_sanitized_text( $parsed[ 'SB_LASTNAME' ] );
-		$list       = $helpers->get_sanitized_text( $parsed[ 'SB_LIST_ID' ] );
+		$folder_id  = $helpers->get_sanitized_text( $parsed[ 'SB_FOLDER_ID' ] );
+		$list_name  = $helpers->get_sanitized_text( $parsed[ 'SB_LIST_NAME' ] );
 
 		if ( empty($email) ) {
 			$this->complete_with_error( $user_id, $action_data, $recipe_id, 'Got empty email from automator' );
 			return;
-		}		
-		
-		$list_ids = [];
-		if ( !empty( $list ) ) {		
-			$list_ids = [ intval( $list ) ];
-		}		
-		
+		}
+
+		if ( empty($folder_id) ) {
+			$this->complete_with_error( $user_id, $action_data, $recipe_id, 'Got empty folder from automator' );
+			return;
+		}
+
+		if ( empty($list_name) ) {
+			$this->complete_with_error( $user_id, $action_data, $recipe_id, 'Got empty list_name from automator' );
+			return;
+		}
+
+		/**
+		 * Check if the waiting list already exists
+		 */
 		try {
-			$helpers->create_or_update_contact($email, $first_name, $last_name, $list_ids);
-			Automator()->complete->action( $user_id, $action_data, $recipe_id );
+			$response = $helpers->get_lists_in_folder(intval($folder_id));
+		} catch (Exception $e) {
+			$this->complete_with_error( $user_id, $action_data, $recipe_id, 'Exception when calling get_lists_in_folder: ' . $e->getMessage() );
+			return;
+		}
+
+		$list_id = NULL;
+		if (!empty($response['lists'])) {
+			foreach ($response['lists'] as $list) {
+				if ($list['name'] == $list_name) {
+					$list_id = $list['id'];
+				}
+			}
+		}		
+
+		// If waiting list was not found, create it
+		if ($list_id == NULL) {			
+			try {
+				$response = $helpers->create_list($list_name, intval($folder_id));
+				$list_id = $response['id'];
+			} catch (Exception $e) {
+				$this->complete_with_error( $user_id, $action_data, $recipe_id, 'Exception when calling create_list: ' . $e->getMessage() );
+				return;
+			}
+		}
+
+		// Create or update contact and add to the waiting list
+		$list_ids = [ $list_id ];
+		try {
+			$helpers->create_or_update_contact($email, $first_name, $last_name, $list_ids);			
 		} catch (Exception $e) {
 			$this->complete_with_error( $user_id, $action_data, $recipe_id, 'Exception when calling create_or_update_contact: ' . $e->getMessage() );
+			return;
 		}
+
+		Automator()->complete->action( $user_id, $action_data, $recipe_id );
 	}
 }
--- sendinblue/helpers/sendinblue-integration-helpers.php
+++ sendinblue/helpers/sendinblue-integration-helpers.php
@@ -1,5 +1,7 @@
 <?php 
 
+require_once(__DIR__ . '/../../vendor/autoload.php');
+
 /**
  * Class Sendinblue_Integration
  */
@@ -15,9 +17,12 @@ class Sendinblue_Integration_Helpers {
 	public function __construct() {
 		// Load settings
 		$this->load_settings();
-
-		include_once __DIR__ . '/class-sendinblue-api.php';
-		$this->api = new Sendinblue_API();
+		
+		$config = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', get_option( 'ua_sendinblue_integration_api_key', '' ) );
+		$this->api = new SendinBlue\Client\Api\ContactsApi(
+			new GuzzleHttp\Client(),
+			$config
+		);
 	}
 
 	/**
@@ -40,5 +45,51 @@ class Sendinblue_Integration_Helpers {
 
 	public function get_sanitized_text($arg) {
 		return sanitize_text_field($this->get_verified_arg( $arg ) );
+	}
+
+	/**
+	 * Sendinblue API
+	 */
+
+	public function create_or_update_contact($email, $first_name, $last_name, $list_ids = []) {
+		if (empty($email)) {
+			throw new Exception('email must be passed to create_or_update_contact function');
+		}
+
+		$createContact = new \SendinBlue\Client\Model\CreateContact();
+		$createContact['email'] = $email;
+		
+		if ( !empty( $list_ids ) ) {		
+			$createContact['listIds'] = $list_ids;
+		}
+
+		$attributes = [];
+        if ( !empty($first_name) ) {
+            $attributes = array_merge( $attributes, [ 'FIRSTNAME' => $first_name ] );
+        }
+
+        if ( !empty($last_name) ) {
+            $attributes = array_merge( $attributes, [ 'LASTNAME' => $last_name ] );
+        }
+
+        if  ( !empty( $attributes ) ) {
+            $createContact['attributes'] = $attributes;
+        }
+
+		$createContact['updateEnabled'] = true;
+				
+		return $this->api->createContact($createContact);		
+	}
+
+	public function get_lists_in_folder($folder_id) {
+		return $this->api->getFolderLists($folder_id, 50, 0);
+	}
+
+	public function create_list($list_name, $folder_id) {
+		$createList = new \SendinBlue\Client\Model\CreateList();
+		$createList['name'] = $list_name;
+		$createList['folderId'] = $folder_id;
+
+		return $this->api->createList($createList);		
 	}
 }
